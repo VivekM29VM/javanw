6d2bffdd0ac44226d79ee8081f531052522eb280
MadankumarK124 <118715280+MadankumarK124@users.noreply.github.com>
2022-11-25 12:42:10 +0530

    Update p2.java

--- src/gromming_abhi/p2.java
+++ src/gromming_abhi/p2.java
@@ -2,13 +2,14 @@ package gromming_abhi;
 //*write a programe to swap 2 numbers  using temporary variable
 public class p2 {
 public static void main(String[] args) {
-	int a=100;
-	int b=300;
-	int tempi
-	tempi=a;
-	a=b;
-	b=tempi;
-	System.out.println(a);
-	System.out.println(b);
+	int r=100;
+	int p=300;
+	int tempi;
+	tempi=r;
+	r=p;
+	p=tempi;
+	System.out.println(r);
+	System.out.println(p);
+	
 }
 }
